2fde1bb6c8aa04ded7bfc949246182ad018734ce
Maza455 <same7nte@gmail.com>
2024-03-07 09:22:37 +0200

    Adding a folder structure

--- index.js
+++ index.js
@@ -1,5 +1,70 @@
-// const word = "SuperCaliFragilistExpalidous"
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import {
+    DB
+} from './config/db.config';
+// import mongoose from 'mongoose';
 
-// const reverseWord = word.split("").reverse("").join("")
+// import routes
+import userRoutes from './routes/userRoutes';
+import traderRoutes from './routes/traderRoutes';
+import customerRoutes from './routes/customerRoutes';
+import productsRoutes from './routes/productsRoutes';
+import ordersRoutes from './routes/ordersRoutes';
 
-// console.log(reverseWord)
+class App {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 5555;
+
+        this.middlewares();
+
+        // import routes
+        this.routes();
+
+        this.db();
+
+        this.listen();
+    }
+
+    middlewares() {
+        this.app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        this.app.use(bodyParser.json({
+            limit: '10mb'
+        }));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes() {
+        // import routes
+        userRoutes(this.app);
+        traderRoutes(this.app);
+        customerRoutes(this.app);
+        productsRoutes(this.app);
+        ordersRoutes(this.app);
+    }
+
+    db() {
+        mongoose.connect(DB.db_url, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            })
+            .then(console.log("Connected successfully"))
+            .catch(err => {
+                console.log("Error message : " + err);
+            });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server is running @ ${this.port}.`);
+        });
+    }
+}
+
+new App();
