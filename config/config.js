711e8c916426fc12301db5871b7c8a6f0d3cce73
Maza455 <same7nte@gmail.com>
2024-03-07 09:43:19 +0200

    Adding a config file

---
+++ config/config.js
@@ -0,0 +1,26 @@
+import {
+    createPool
+} from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = createPool({
+    database: process.env.DB_Name,
+    host: process.env.DB_HOST,
+    user: process.env.DB_UserName,
+    password: process.env.DB_UserPass,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    multipleStatements: true,
+    connectionLimit: 30
+});
+
+const authentication = {
+    jwtSecret: process.env.SECRET_KEY
+};
+
+export default {
+    connection,
+    authentication
+};
