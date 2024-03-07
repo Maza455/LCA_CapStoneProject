712e116efce38e02168e06c542f6543f8d03a6e4
Maza455 <same7nte@gmail.com>
2024-03-07 08:56:17 +0200

    Folder Structure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const word = "SuperCaliFragilistExpalidous"
+// const word = "SuperCaliFragilistExpalidous"
 
-const reverseWord = word.split("").reverse("").join("")
+// const reverseWord = word.split("").reverse("").join("")
 
-console.log(reverseWord)
+// console.log(reverseWord)
